18812993c34f3d46cd00a1672fa9e6e093dfceb1
RF-Rezon <rezwanfaysalrezon@gmail.com>
2025-08-28 21:51:01 +0600

    DFS Traversal On 2D Grid

--- Grid/2D/DFS_Traversal_on_2D_Grid.cpp
+++ Grid/2D/DFS_Traversal_on_2D_Grid.cpp
@@ -1,26 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n, m;
 char grid[1005][1005];
 bool visited[1005][1005];
 vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 
+bool valid(int i, int j)
+{
+    if (i < 0 || j < 0 || i >= n || j >= m)
+        return false;
+    return true;
+}
+
 void bfs(int si, int sj)
 {
-    cout << "Source:" << si << " " << sj << endl;
+    cout << si << " " << sj << endl;
     visited[si][sj] = true;
     for (int i = 0; i < 4; i++)
     {
         int ci, cj;
         ci = si + d[i].first;
         cj = sj + d[i].second;
-        cout << ci << " " << cj << endl;
+        if (valid(ci, cj) && !visited[ci][cj])
+            bfs(ci, cj);
     }
 }
 
 int main()
 {
-    int n, m;
     cin >> n >> m;
     for (int i = 0; i < n; i++)
     {
@@ -31,6 +39,7 @@ int main()
     }
     int si, sj;
     cin >> si >> sj;
+    memset(visited, false, sizeof(visited));
     bfs(si, sj);
     return 0;
 }
